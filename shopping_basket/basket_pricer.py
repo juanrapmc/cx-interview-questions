e5aafe0ed057da518c2ded309365697b7ef6808b
rap-rog <juanrapmcornejo@gmail.com>
2020-10-09 17:11:17 +0100

    Added test to get basic total

---
+++ shopping_basket/basket_pricer.py
@@ -0,0 +1,9 @@
+
+
+class BasketPricer(object):
+
+    def __init__(self, catalogue, offers=None, basket=None):
+        pass
+
+    def computeTotal(self):
+        pass
--- shopping_basket/shopping_basket_tests/test_basket_pricer.py
+++ shopping_basket/shopping_basket_tests/test_basket_pricer.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from basket_pricer import BasketPricer
 
 catalogue = {
     101: {'name': 'Baked Beans', 'price': 0.99},
@@ -9,8 +10,25 @@ catalogue = {
     353: {'name': 'Shampoo (Large)', 'price': 3.50},
 }
 
+offers = {
+    101: [("buyXgetX", 2, 1),],
+    200: [("percentOff", 25)],
+    351: [("buyXSetGetMinFree", 1)],
+    352: [("buyXSetGetMinFree", 1)],
+    353: [("buyXSetGetMinFree", 1)],
+}
+
 
 class TestBasketPricer(TestCase):
 
     def test(self):
         self.assertTrue(True)
+
+    def test_get_basket_total_no_offers(self):
+        basket = [
+            (101, 3),
+            (150, 2)
+        ]
+        bpricer = BasketPricer(catalogue, basket=basket)
+        self.assertEqual(5.37, bpricer.computeTotal())
+        pass
