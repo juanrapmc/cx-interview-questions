7cee9c52f53c2d6d8bb65d9cf7fe45053feb23d3
rap-rog <juanrapmcornejo@gmail.com>
2020-10-09 18:38:48 +0100

    Added logic to compute basket and subtotal

--- shopping_basket/basket_pricer.py
+++ shopping_basket/basket_pricer.py
@@ -8,10 +8,27 @@ class BasketPricer(object):
         self.basket = basket
 
     def computeBasket(self):
-        pass
+        summary = {
+            'subtotal': 0,
+            'discount': 0,
+            'total': 0
+        }
+
+        if not self.basket:
+            return summary
+        if self.offers:
+            summary['discount'] = self.computeDiscount()
+
+        summary['subtotal'] = self.computeSubtotal()
+        summary['total'] = summary['subtotal'] - summary['discount']
+        return summary
 
     def computeSubtotal(self):
-        pass
+        subtotal = 0
+        for item, qty in self.basket:
+            if item in self.catalogue:
+                subtotal += self.catalogue[item]['price']*qty
+        return subtotal
 
     def computeDiscount(self):
         pass
