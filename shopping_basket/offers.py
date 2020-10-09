1c62b5776aa74a57a3a5861f65eb7a598420e279
rap-rog <juanrapmcornejo@gmail.com>
2020-10-10 00:19:27 +0100

    Added implementation for BuyXGetXOffer

--- shopping_basket/offers.py
+++ shopping_basket/offers.py
@@ -27,4 +27,21 @@ class PercentOffOffer(Offer):
 class BuyXGetXOffer(Offer):
 
     def computeDiscount(self):
-        pass
+        discount = 0
+        for item, qty in self.basket:
+            if item in self.offers:
+                for offer in self.offers[item]:
+                    if offer[0] == 'BuyXGetXOffer':
+                        buy_amt = 0
+                        free_amt = 0
+                        free_total = 0
+                        for _ in range(1, qty+1):
+                            if free_amt:
+                                free_total += 1
+                                free_amt -= 1
+                                continue
+                            buy_amt += 1
+                            if buy_amt % offer[1] == 0:
+                                free_amt = offer[2]
+                        discount += self.catalogue[item]['price'] * free_total
+        return round(discount, 2)
