262fb9f7c8a8c138fecda936210b7da588f257b7
rap-rog <juanrapmcornejo@gmail.com>
2020-10-10 12:51:18 +0100

    Refactored offers - added abstract method compute to contain business logic

--- shopping_basket/offers.py
+++ shopping_basket/offers.py
@@ -9,39 +9,37 @@ class Offer(ABC):
         self.basket = basket
 
     @abstractmethod
-    def computeDiscount(self):
+    def compute(self, offer, item, qty):
         raise NotImplementedError
+
+    def computeDiscount(self):
+        discount = 0
+        for item, qty in self.basket:
+            if item in self.offers:
+                for offer in self.offers[item]:
+                    if offer[0] == type(self).__name__:
+                        discount += self.compute(offer, item, qty)
+        return round(discount, 2)
 
 
 class PercentOffOffer(Offer):
 
-    def computeDiscount(self):
-        discount = 0
-        for item, qty in self.basket:
-            if item in self.offers:
-                for offer in self.offers[item]:
-                    if offer[0] == 'PercentOffOffer':
-                        discount += self.catalogue[item]['price'] * qty * (offer[1]/100)
-        return round(discount, 2)
+    def compute(self, offer, item, qty):
+        return self.catalogue[item]['price'] * qty * (offer[1]/100)
+
 
 class BuyXGetXOffer(Offer):
 
-    def computeDiscount(self):
-        discount = 0
-        for item, qty in self.basket:
-            if item in self.offers:
-                for offer in self.offers[item]:
-                    if offer[0] == 'BuyXGetXOffer':
-                        buy_amt = 0
-                        free_amt = 0
-                        free_total = 0
-                        for _ in range(1, qty+1):
-                            if free_amt:
-                                free_total += 1
-                                free_amt -= 1
-                                continue
-                            buy_amt += 1
-                            if buy_amt % offer[1] == 0:
-                                free_amt = offer[2]
-                        discount += self.catalogue[item]['price'] * free_total
-        return round(discount, 2)
+    def compute(self, offer, item, qty):
+        buy_amt = 0
+        free_amt = 0
+        free_total = 0
+        for _ in range(1, qty+1):
+            if free_amt:
+                free_total += 1
+                free_amt -= 1
+                continue
+            buy_amt += 1
+            if buy_amt % offer[1] == 0:
+                free_amt = offer[2]
+        return self.catalogue[item]['price'] * free_total
